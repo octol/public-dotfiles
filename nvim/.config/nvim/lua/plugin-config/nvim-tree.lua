38b63c69ef7a80abb7cb4121ef6d04fe44b9a48d
Jon Häggblad <jon.haggblad@gmail.com>
2023-11-02 11:32:04 +0100

    Try another tmux binding

--- nvim/.config/nvim/lua/plugin-config/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugin-config/nvim-tree.lua
@@ -12,12 +12,12 @@ require("nvim-tree").setup()
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
-    width = 30,
+    width = 40,
   },
   renderer = {
     group_empty = true,
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
   },
 })
