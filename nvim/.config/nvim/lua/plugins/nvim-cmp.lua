5c430998cccd4a37c3616318e61ff9c8b4444531
Jon Häggblad <jon.haggblad@gmail.com>
2024-09-27 14:53:40 +0200

    Add nvim-cmp

---
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,78 @@
+return {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        {
+            "L3MON4D3/LuaSnip",
+            -- follow latest release.
+            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+            -- install jsregexp (optional!).
+            build = "make install_jsregexp"
+        },
+    },
+    event = "InsertEnter",
+    config = function()
+        local cmp = require 'cmp'
+        local luasnip = require 'luasnip'
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = {
+                ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+                ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+                ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+                ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+                ['<C-e>'] = cmp.mapping({
+                    i = cmp.mapping.abort(),
+                    c = cmp.mapping.close(),
+                }),
+                ['<CR>'] = cmp.mapping.confirm({
+                    select = false,
+                }),
+                -- Additional next_item
+                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-n>'] = cmp.mapping.select_next_item(),
+                -- Add tab support
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+            },
+            -- window = {
+            --     completion = cmp.config.window.bordered(),
+            --     documentation = cmp.config.window.bordered(),
+            -- },
+            -- formatting = {
+            --     fields = {'menu', 'abbr', 'kind'},
+            --     format = function(entry, item)
+            --         local menu_icon ={
+            --             nvim_lsp = 'λ',
+            --             vsnip = '⋗',
+            --             buffer = 'Ω',
+            --             path = '🖫',
+            --         }
+            --         item.menu = menu_icon[entry.source.name]
+            --         return item
+            --     end,
+            -- },
+
+            -- Installed sources
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                -- { name = 'vsnip' }, -- For vsnip users.
+                { name = 'crates' },
+                { name = 'luasnip' }, -- For luasnip users.
+                -- { name = 'ultisnips' }, -- For ultisnips users.
+                -- { name = 'snippy' }, -- For snippy users.
+            }, {
+                { name = 'buffer' },
+            })
+        })
+    end,
+}
