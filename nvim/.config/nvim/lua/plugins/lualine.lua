d1a57470d7d97a88a9fdcda5851620f38f2b7983
Jon Häggblad <jon.haggblad@gmail.com>
2025-04-22 22:46:01 +0200

    Use gitsigns in lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,23 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'lewis6991/gitsigns.nvim' },
+
     config = function()
+        local function diff_source()
+            local gitsigns = vim.b.gitsigns_status_dict
+            if gitsigns then
+                return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed
+                }
+            end
+        end
+
         require('lualine').setup {
             sections = {
-                lualine_b = {'diff', 'diagnostics'},
+                lualine_b = {{'diff', source = diff_source}, 'diagnostics'},
                 lualine_c = {{'filename', path = 1}},
             },
             inactive_sections = {
