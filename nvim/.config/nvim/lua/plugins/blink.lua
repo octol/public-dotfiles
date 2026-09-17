a20de2ec95df7ac135ec79fc8b99f7d0e96e8df7
Jon Häggblad <jon.haggblad@gmail.com>
2026-09-17 22:14:59 +0200

    fix: nvim cmdline completion

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -15,6 +15,18 @@ return {
         preset = "super-tab",
         ["<C-y>"] = { "select_and_accept", "fallback" },
       },
+      cmdline = {
+        keymap = {
+          -- Share buffer completion navigation, including Up/Down.
+          preset = "inherit",
+          -- Also let Tab open completion when the menu is hidden.
+          ["<Tab>"] = { "select_and_accept", "show_and_insert_or_accept_single", "fallback" },
+        },
+        completion = {
+          menu = { auto_show = false },
+          ghost_text = { enabled = false },
+        },
+      },
       snippets = { preset = "luasnip" },
       -- sources.default is already { "lsp", "path", "snippets", "buffer" };
     },
